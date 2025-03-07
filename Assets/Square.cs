cc019e55a52c04a2b75d064b2fa0a0f0b098729f
u7947637@anu.edu.au <u7947637@anu.edu.au>
2025-03-07 16:33:07 +1100

    Added Comments to Square.cs

--- Assets/Square.cs
+++ Assets/Square.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/*
+This class is for the submarine of the game. Even though it is named Square
+*/
 public class Square : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,14 +25,17 @@ public class Square : MonoBehaviour
          UnityEngine.Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 
+
+        //Implements the rotation of the sub
+
         UnityEngine.Vector2 direction =  new UnityEngine.Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
 
         transform.up = direction; 
         
+
+
+        //Implements the movement of the Sub 
         UnityEngine.Vector3 target = new UnityEngine.Vector3(mousePos.x, transform.position.y, transform.position.z);
-
-       
-
         this.transform.position = UnityEngine.Vector3.Lerp(this.transform.position,target, 5f*Time.deltaTime); 
 
         if (Input.GetKeyDown(KeyCode.Space)) {

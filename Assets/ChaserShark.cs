23a749c17f3fde2e7db9c7232af4d19652914d4f
SamJoffy <114778680+SamJoffy@users.noreply.github.com>
2025-03-07 12:57:02 +1100

    Added movement code to chasershark

--- Assets/ChaserShark.cs
+++ Assets/ChaserShark.cs
@@ -1,18 +1,22 @@
+using System;
 using UnityEngine;
 
 public class ChaserShark : MonoBehaviour
 {
-
+    public GameObject player;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float speed = 1.0F;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(player.transform.position.x, transform.position.y + speed * Time.deltaTime * Vector3.Normalize(player.transform.position - transform.position).y, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("balls");
+        }
     }
 }

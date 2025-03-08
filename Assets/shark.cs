fed7854319417e716b276fdc3d59d50d7e75438b
u7947637@anu.edu.au <u7947637@anu.edu.au>
2025-03-08 15:56:17 +1100

    Added Shark

---
+++ Assets/shark.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class shark : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject player; 
+
+    private float speed = 1.0f; 
+
+    private Rigidbody2D rb; 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector2 direction = (player.transform.position - transform.position).normalized; 
+        
+        movePlayer(direction); 
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("balls");
+        }
+    }
+
+    void movePlayer(Vector2 direction) {
+        rb.linearVelocity = new Vector2(direction.x, direction.y) * speed; 
+    }
+}
